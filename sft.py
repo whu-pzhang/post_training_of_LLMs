5279fbd52617502d708d7763bec3507440aa12f3
whu-pzhang <pzhang@whu.edu.cn>
2025-07-30 10:48:11 +0800

    update

--- sft.py
+++ sft.py
@@ -1,36 +1,104 @@
-from datasets import Dataset, load_dataset
-from transformers import (AutoModelForCausalLM,
-                          AutoModelForSequenceClassification, AutoTokenizer,
-                          Trainer, TrainingArguments)
-from trl import DataCollatorForCompletionOnlyLM, SFTConfig, SFTTrainer
+"""
+# Full training
+```
+python sft.py \
+    --model_name_or_path Qwen/Qwen2-0.5B \
+    --dataset_name trl-lib/Capybara \
+    --learning_rate 2.0e-5 \
+    --num_train_epochs 1 \
+    --packing \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 8 \
+    --gradient_checkpointing \
+    --eos_token '<|im_end|>' \
+    --eval_strategy steps \
+    --eval_steps 100 \
+    --output_dir Qwen2-0.5B-SFT \
+    --push_to_hub
+```
+"""
 
-from helper import display_dataset, load_model_and_tokenizer
+import argparse
 
-USE_GPU = True
-
-model_name = "HuggingFaceTB/SmolLM2-135M"
-# model_name = "Qwen/Qwen3-0.6B-Base"
-model, tokenizer = load_model_and_tokenizer(model_name, use_gpu=USE_GPU)
-
-train_dataset = load_dataset("banghua/DL-SFT-Dataset")["train"]
-display_dataset(train_dataset)
-
-sft_config = SFTConfig(
-    learning_rate=8e-5,
-    num_train_epochs=1,
-    per_device_train_batch_size=1,
-    gradient_accumulation_steps=8,
-    gradient_checkpointing=False,
-    logging_steps=10,
+from datasets import load_dataset
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+from trl import (
+    ModelConfig,
+    ScriptArguments,
+    SFTConfig,
+    SFTTrainer,
+    TrlParser,
+    clone_chat_template,
+    get_kbit_device_map,
+    get_peft_config,
+    get_quantization_config,
 )
 
-sft_trainer = SFTTrainer(
-    model=model,
-    args=sft_config,
-    train_dataset=train_dataset,
-    processing_class=tokenizer,
-)
-sft_trainer.train()
 
-# save the model
-sft_trainer.save_model("models/SmolLM2-135M-SFT")
+def main(script_args, training_args, model_args):
+    ################
+    # Model init kwargs & Tokenizer
+    ################
+    quantization_config = get_quantization_config(model_args)
+
+    model_kwargs = dict(
+        trust_remote_code=model_args.trust_remote_code,
+        attn_implementation=model_args.attn_implementation,
+        torch_dtype=model_args.torch_dtype,
+        use_cache=False if training_args.gradient_checkpointing else True,
+        device_map=get_kbit_device_map() if quantization_config is not None else None,
+        quantization_config=quantization_config,
+    )
+
+    # create model
+    config = AutoConfig.from_pretrained(model_args.model_name_or_path)
+    model = AutoModelForCausalLM.from_pretrained(model_args.model_name_or_path, **model_kwargs)
+
+    # create tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path, trust_remote_code=model_args.trust_remote_code, use_fast=True
+    )
+    if tokenizer.chat_template is None:
+        model, tokenizer = clone_chat_template(model, tokenizer, "Qwen/Qwen3-0.6B")
+
+    ################
+    # Dataset
+    ################
+    dataset = load_dataset(script_args.dataset_name, name=script_args.dataset_config)
+
+    ################
+    # Training
+    ################
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset[script_args.dataset_train_split],
+        eval_dataset=dataset[script_args.dataset_test_split] if training_args.eval_strategy != "no" else None,
+        processing_class=tokenizer,
+        peft_config=get_peft_config(model_args),
+    )
+
+    trainer.train()
+
+    # Save and push to hub
+    trainer.save_model(training_args.output_dir)
+    if training_args.push_to_hub:
+        trainer.push_to_hub(dataset_name=script_args.dataset_name)
+
+
+def make_parser(subparsers: argparse._SubParsersAction = None):
+    dataclass_types = (ScriptArguments, SFTConfig, ModelConfig)
+    if subparsers is not None:
+        parser = subparsers.add_parser("sft", help="Run the SFT training script", dataclass_types=dataclass_types)
+    else:
+        parser = TrlParser(dataclass_types)
+    return parser
+
+
+if __name__ == "__main__":
+    parser = make_parser()
+    # When using the trl cli, this script may be run with additional arguments, corresponding accelerate arguments.
+    # To ensure that their parsing does not interfere with the script arguments, parse the arguments with
+    # `return_remaining_strings=True`, then ignore the remaining strings.
+    script_args, training_args, model_args, _ = parser.parse_args_and_config(return_remaining_strings=True)
+    main(script_args, training_args, model_args)
